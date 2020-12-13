d8954969d62c94a6b0d67f4fab03dcc8bcfc96e7
Hannah <hannah.n.kahnwald@gmail.com>
2020-12-13 00:18:52 -0600

    bucle

--- public/web-server.js
+++ public/web-server.js
@@ -150,6 +150,29 @@
           </CardActions>
         </Card>
       </div>
+      <div className={classes.top}>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              alt='Contemplative Reptile'
+              image='https://www.trustedreviews.com/wp-content/uploads/sites/54/2019/09/image011-1-1220x851.png'
+              title='Contemplative Reptile'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='h2'>
+                Change color
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                You can select the color, click in button below and choose
+                the right color, then set the color intensity.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.hannahContainer}>
+          </CardActions>
+        </Card>
+      </div>
     </Container>
     );
   }
